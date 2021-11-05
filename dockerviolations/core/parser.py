e0e431b52519a87acd975f8091c73cfffe914e64
dheeruymv <65417565+dheeruymv@users.noreply.github.com>
2021-11-05 18:38:05 +0530

    Multiple LABELS command recommendation
    When Docker file has multiple LABELS based on the Docker version 1.10, recommendation support has been added.

--- dockerviolations/core/parser.py
+++ dockerviolations/core/parser.py
@@ -22,10 +22,12 @@ class RuleEngine:
     def __init__(self, content_list):
         self._docker_content = content_list
         self._docker_rules = {
-            'mkdir': "Group all 'mkdir' commands into single layer",
+            'mkdir':
+            "Group all 'mkdir' commands into single layer",
             'label_not_present':
             "Use LABEL Command to organize images by project, recording license info, to aid in automation",
-            'label': ""
+            'multiple_labels':
+            "If docker version < 1.10: Merge multiple LABEL commands to single LABEL command. If docker version >=1.10: ignore the recommendation, but as good practice merge LABEL to single command"
         }
 
     def get_violations(self):
@@ -38,17 +40,7 @@ class RuleEngine:
                 "Violation": "MKDIR command can be improved",
                 "Recommendation": self._docker_rules['mkdir']
             })
-        if not self._is_label_present():
-            violations.append({
-                "Line #":
-                self._get_line_number("label"),
-                "Violation":
-                "LABEL command is not present",
-                "Recommendation":
-                self._docker_rules['label_not_present']
-            })
-        else:
-            pass
+        violations.append(self._get_label_violations())
         return violations
 
     def _is_entrypoint_or_cmd_present(self):
@@ -64,12 +56,27 @@ class RuleEngine:
                 counter += 1
         return True if counter > 1 else False
 
-    def _is_label_present(self):
+    def _get_label_violations(self):
+        label_counter = self._get_label_counter()
+        if label_counter == 0:
+            return {
+                "Line #": self._get_line_number("LABEL"),
+                "Violation": "LABEL command is not present",
+                "Recommendation": self._docker_rules['label_not_present']
+            }
+        elif label_counter > 1:
+            return {
+                "Line #": self._get_line_number("LABEL"),
+                "Violation": "Multiple LABEL commands are present",
+                "Recommendation": self._docker_rules['multiple_labels']
+            }
+
+    def _get_label_counter(self):
         counter = 0
         for docker_command in self._docker_content:
-            if "label" in docker_command:
+            if "label".upper() in docker_command:
                 counter += 1
-        return True if counter >= 1 else False
+        return counter
 
     def _get_line_number(self, pattern):
         line_numbers = ""
